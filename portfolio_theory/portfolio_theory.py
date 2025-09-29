b2b1aada0ed9f8f29078cb25ee7607c422622209
Moksh Chadha <mokshchadha@Mokshs-MacBook-Pro.local>
2025-09-29 20:54:13 +0530

    formulae for expected returns and volaitily added

--- portfolio_theory/portfolio_theory.py
+++ portfolio_theory/portfolio_theory.py
@@ -13,6 +13,7 @@ stocks = ['RELIANCE.NS', 'TCS.BO', 'INFY.NS', 'ICICIBANK.NS', 'SBIN.NS', 'PNB.NS
 # historical data - define start and end dates (using past dates with actual trading data)
 start_date = '2025-01-01'
 end_date = '2025-09-01'
+NUM_TRADING_DAYS = 9 * 20 # trading days are the days on which u can trade
 
 def download_data():
     stock_dataframes = []
@@ -29,16 +30,38 @@ def download_data():
     return df
 
 def show_data(data):
-    data.plot(figsize=(10,5))
+    data.plot(figsize=(10,6))
     plt.show()
+
+def calculate_return(data):
+    # data 1 2 3 4 5
+    #.       1 2 3 5 
+    # hence we arrive at s(t+1) / s(t) -- gross return of a price
+    log_return = np.log(data/ data.shift(1)) #log return --> normalisatoin
+    return log_return[1:] # because 1 row has nothing to be subratcted from 
+
+def show_statistics(returns):
+    print(returns.mean() * NUM_TRADING_DAYS)
+    print(returns.cov() * NUM_TRADING_DAYS)
+
+def show_mean_variance(returns, weights):
+    weights = np.array(weights)
+    # we are after the annual returns and not daily
+    portfolio_return = np.sum(returns.mean() * weights) * NUM_TRADING_DAYS # -- imp formulae
+    transpose_weights = weights.T
+    portfolio_volatily_square =  np.dot(transpose_weights, np.dot(returns.cov() * NUM_TRADING_DAYS, weights)) # wt . E w -- imp formula
+    portfolio_volatily = np.sqrt(portfolio_volatily_square)
+
+    print("expected returns : ", portfolio_return)
+    print("expected volatitly :", portfolio_volatily)
+
 
 def main():
     stock_data = download_data()
-    print("Stock Price Data:")
-    print(stock_data.head())
-    print(f"\nShape: {stock_data.shape}")
-    print(f"Date range: {stock_data.index[0]} to {stock_data.index[-1]}")
-    show_data(stock_data)
+    # show_data(stock_data)
+    returns = calculate_return(stock_data)
+    show_statistics(returns)
+    show_mean_variance(returns, [0.3, 0.2, 0.2, 0.1, 0.1 , 0.1])
 
 if __name__ == "__main__":
     main()
