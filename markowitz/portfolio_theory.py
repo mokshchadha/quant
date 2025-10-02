bbc54a866b31ee45491d66363735cfd3b14ff408
Moksh Chadha <mokshchadha@Mokshs-MacBook-Pro.local>
2025-10-02 12:57:49 +0530

    markowitz model complete

--- markowitz/portfolio_theory.py
+++ markowitz/portfolio_theory.py
@@ -82,6 +82,33 @@ def show_portfolios(returns, volatilities):
     fig.colorbar(scatter, ax=ax, label='Sharpe Ratio')
     plt.show()  # This was missing!
 
+def statistics(weights, returns):
+    portfolio_return = np.sum(returns.mean() * weights) * NUM_TRADING_DAYS
+    portfolio_risk = np.sqrt(np.dot(weights.T, np.dot(returns.cov()* NUM_TRADING_DAYS, weights))) # squre root of dot product of weight tranpose with (dot product of covarians and weights)
+    sharpe_ratio = portfolio_return/portfolio_risk
+    return np.array([portfolio_return, portfolio_risk, sharpe_ratio])
+
+#optimisation - scipy optimize can find the minimum of a function
+# the maximum of a f(x) is the minum of -f(x)
+def min_function_sharpe(weights, returns):
+    return -1 * statistics(weights, returns)[2]
+
+def constraints1(weights):
+    #what is the constraint in markowitz -- the sum of weights should be 1
+    # sum of w = 1 ie. sum of w -1 = fx =0 is the function to be minimized
+    return np.sum(weights) - 1
+
+def optimize_portfolio(weights, returns):
+    constraints = {'type':'eq', 'fun': constraints1}
+    bound = (0.0, 1.0) # the weight can be from 0 to 1 and there are 
+    bounds = tuple(bound for _ in range(len(stocks)))
+    intialWeights = weights[0]
+    return optimization.minimize(fun=min_function_sharpe, x0=intialWeights, args=returns, method='SLSQP', bounds=bounds, constraints=[constraints])
+
+
+def print_optimal_porfolio(optimum, returns):
+    print("Optimal portfolio", optimum['x'].round(3))
+    print("Expected return , volatily and sharpe ratio", statistics(optimum['x'].round(3), returns))
 
 def main():
     stock_data = download_data()
@@ -89,7 +116,9 @@ def main():
     returns = calculate_return(stock_data)
     show_statistics(returns)
     weights, means, risks = generate_portfolios(returns)
-    show_portfolios(means, risks)
+    # show_portfolios(means, risks)
+    optimum = optimize_portfolio(weights, returns)
+    print_optimal_porfolio(optimum, returns)
 
 
 if __name__ == "__main__":
