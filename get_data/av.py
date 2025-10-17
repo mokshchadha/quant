82206914dfbe73ca60e4f0027856dafd3a1b7a16
Moksh Chadha <mokshchadha@Mokshs-MacBook-Pro.local>
2025-10-17 23:07:17 +0530

    v0 of alpha vantage added

---
+++ get_data/av.py
@@ -0,0 +1,26 @@
+from alpha_vantage.timeseries import TimeSeries
+import pandas as pd
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv("ALPHA_VANTAGE_KEY")
+
+ts = TimeSeries(api_key, 'pandas')
+data = ts.get_daily(symbol="EURUSD", outputsize='full')[0]
+data.columns = ['open', 'close', 'high', 'low', 'volume']
+
+all_tickers = ["AAPL", "MSFT", "CSCO", "AMZN", "FB", "GOOG"]
+close_prices = pd.DataFrame()
+
+for ticker in all_tickers:
+    data = ts.get_intraday(symbol=ticker, interval="1min", outputsize="full")[0]
+    data.columns = ['open', 'close', 'high', 'low', 'volume']
+    close_prices[ticker] = data['close']
+
+
+
+print(close_prices)
+
+
